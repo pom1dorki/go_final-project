420236181e8d91f1fd9f55dc505f75225c2e11b7
Ilya Petrischev <pom1dorki@yandex.ru>
2026-04-22 13:55:45 +0300

    Add tasks list

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -7,6 +7,7 @@ const DateFormat = "20060102"
 func Init() {
 	http.HandleFunc("/api/nextdate", nextDateHandler)
 	http.HandleFunc("/api/task", taskHandler)
+	http.HandleFunc("/api/tasks", tasksHandler)
 }
 
 func taskHandler(w http.ResponseWriter, r *http.Request) {
--- pkg/db/task.go
+++ pkg/db/task.go
@@ -7,3 +7,31 @@ type Task struct {
 	Comment string `json:"comment"`
 	Repeat  string `json:"repeat"`
 }
+
+func Tasks(limit int) ([]*Task, error) {
+	query := `SELECT id, date, title, comment, repeat 
+              FROM scheduler 
+              ORDER BY date ASC 
+              LIMIT ?`
+
+	rows, err := db.Query(query, limit)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tasks []*Task
+	for rows.Next() {
+		var t Task
+		if err := rows.Scan(&t.ID, &t.Date, &t.Title, &t.Comment, &t.Repeat); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, &t)
+	}
+
+	if tasks == nil {
+		tasks = []*Task{}
+	}
+
+	return tasks, rows.Err()
+}
