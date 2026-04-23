07ec472382c9571d35dbbf901fe52d5c14fc02e1
Ilya Petrischev <pom1dorki@yandex.ru>
2026-04-23 12:44:57 +0300

    Add mark task as done + delete task endpoints

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -8,6 +8,7 @@ func Init() {
 	http.HandleFunc("/api/nextdate", nextDateHandler)
 	http.HandleFunc("/api/task", taskHandler)
 	http.HandleFunc("/api/tasks", tasksHandler)
+	http.HandleFunc("/api/task/done", doneHandler)
 }
 
 func taskHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +19,8 @@ func taskHandler(w http.ResponseWriter, r *http.Request) {
 		getTaskHandler(w, r)
 	case http.MethodPut:
 		updateTaskHandler(w, r)
+	case http.MethodDelete:
+		deleteTaskHandler(w, r)
 	default:
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 	}
--- pkg/db/task.go
+++ pkg/db/task.go
@@ -65,3 +65,23 @@ func UpdateTask(task *Task) error {
 	}
 	return nil
 }
+
+func DeleteTask(id string) error {
+	res, err := db.Exec(`DELETE FROM scheduler WHERE id = ?`, id)
+	if err != nil {
+		return err
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if count == 0 {
+		return fmt.Errorf("task not found")
+	}
+	return nil
+}
+
+func UpdateDate(next string, id string) error {
+	_, err := db.Exec(`UPDATE scheduler SET date = ? WHERE id = ?`, next, id)
+	return err
+}
